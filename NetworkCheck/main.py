cddc9ce88d3da0aa837c5312eaf8001c06cb6896
薛云飞 <564354487@qq.com>
2018-10-16 14:05:36 +0800

    ver1.0--add comments2

--- NetworkCheck/main.py
+++ NetworkCheck/main.py
@@ -12,32 +12,33 @@ import time
 
 def baseparameters():       # 一些基础参数的获取
     while 1:
-        host = raw_input(u'请确认所有linux主机的用户名和密码一致，是否继续（y/n）: ')
+        host = raw_input('请确认所有linux主机的用户名,密码和ssh登陆端口一致，是否继续（y/n）: ')
         if host == 'y' or host == 'Y':
-            global username, password, localhosts, remotehosts, telnetip, telnetports
-            username = raw_input(u'请输入用户名：')
-            password = raw_input(u'请输入密码：')
+            global username, password, loginport, localhosts, remotehosts, telnetip, telnetports
+            username = raw_input('请输入用户名：')
+            password = raw_input('请输入密码：')
+            loginport = raw_input('请输入登陆端口：')
             localhosts = {}
             remotehosts = {}
-            host = raw_input(u'请输入本机的ip地址：')
-            hosts = raw_input(u'请输入远程登陆的ip地址，以空格分开：')
+            host = raw_input('请输入本机的ip地址：')
+            hosts = raw_input('请输入远程登陆的ip地址，以空格分开：')
             localhosts[host] = password
             for h in hosts.split():
                 remotehosts[h] = password
-            telnetip = raw_input(u'请输入端口检测的ip：')
-            telnetports = raw_input(u'请输入需要检测的端口，以空格区分：')
+            telnetip = raw_input('请输入端口检测的ip：')
+            telnetports = raw_input('请输入需要检测的端口，以空格区分：')
         else:
-            print u"你需要先设置用户名和密码，确保他们在所有linux主机上一致"
+            print "你需要先设置用户名和密码，确保他们在所有linux主机上一致"
             exit(5)
-        ifcontinous = raw_input(u'请确认以上输入是否正确（y/n）: ')
+        ifcontinous = raw_input('请确认以上输入是否正确（y/n）: ')
         if ifcontinous == 'y' or host == 'Y':
             break
         else:
             continue
 
 
-def pingstatus(username, localhosts, remotehosts):      # ping检测
-    print "开始进行ping值检测..."
+def pingstatus(username, localhosts, remotehosts, pingport=22):      # ping检测
+    print "开始进行ping值检测...请稍候"
     pinghost = ["114.114.114.114", "www.baidu.com"]  # ping状态获取
     remote_ping_cmd = []
     pingresult = []
@@ -51,7 +52,7 @@ def pingstatus(username, localhosts, remotehosts):      # ping检测
     allhosts.update(remotehosts)
     for host, pd in allhosts.items():    # 登陆每台服务器执行操作
         remote_ping_run = ServerLogin(host, pd)
-        a = threading.Thread(target=remote_ping_run.sshlogin, args=(username, remote_ping_cmd))
+        a = threading.Thread(target=remote_ping_run.sshlogin, args=(username, remote_ping_cmd, pingport))
         a.start()
         a.join()
     while not parameters.get_value('qping').empty():        # 处理上面操作完成后队列中的数据
@@ -72,10 +73,9 @@ def pingstatus(username, localhosts, remotehosts):      # ping检测
     #               '192.168.0.180':[['100%', 'null', 'null', 'null'], ['0%', '7.8 ms', '88.5 ms', '9.0 ms']]}"
 
 
-def telnetstatus(username, telnetip, telnetports):      # 端口检测
-    print "开始检测对外开放的端口..."
+def telnetstatus(username, telnetip, telnetports, telnetport=22):      # 端口检测
+    print "开始检测对外开放的端口...请稍候"
     telnetlocalhosts = {'192.168.0.163': 'aykj83752661'}   # 远程登陆执行telnet命令的服务器
-    telnetport = '22'  # 远程登陆执行telnet命令的服务器的端口
     remote_telnet_cmd = []
     portresult = []
     portconnect = {}
@@ -95,8 +95,8 @@ def telnetstatus(username, telnetip, telnetports):      # 端口检测
     # "portconnect": "{'8000': u'\u4e0d\u53ef\u8fbe','7001': u'\u53ef\u8fbe'}"
 
 
-def getband(username, localhosts, remotehosts):     # 带宽检测
-    print '开始检测服务器之间的带宽...'
+def getband(username, localhosts, remotehosts, bandport=22):     # 带宽检测
+    print "开始检测服务器之间的带宽...请稍候"
     remote_cmd = [  # band状态获取
         'if [ `dpkg -l|grep iperf|wc -l` = 0 ];then echo aykj83752661 |sudo -S apt-get install iperf -y --force-yes;fi',
         'iperf -s -D 1>&2']  # 根据返回信息判断，当存在返回信息时，会一直判断处于运行状态，不退出
@@ -114,8 +114,8 @@ def getband(username, localhosts, remotehosts):     # 带宽检测
                 'iperf -c ' + host + ' -t 6 -i 3']
             remote_run = ServerLogin(host, pd)
             local_run = ServerLogin(localhost, localpd)
-            c = threading.Thread(target=remote_run.sshlogin, args=(username, remote_cmd))
-            d = threading.Thread(target=local_run.sshlogin, args=(username, local_cmd))
+            c = threading.Thread(target=remote_run.sshlogin, args=(username, remote_cmd, bandport))
+            d = threading.Thread(target=local_run.sshlogin, args=(username, local_cmd, bandport))
             c.start()
             c.join()
             d.start()
@@ -159,9 +159,9 @@ if __name__ == '__main__':      # 主函数
     parameters.set_value('q', Queue.Queue())
     parameters.set_value('qping', Queue.Queue())
     parameters.set_value('qport', Queue.Queue())
-    pingvalue = pingstatus(username, localhosts, remotehosts)
-    band, hostip = getband(username, localhosts, remotehosts)
-    portvalue = telnetstatus(username, telnetip, telnetports)
+    pingvalue = pingstatus(username, localhosts, remotehosts, int(loginport))
+    band, hostip = getband(username, localhosts, remotehosts, int(loginport))
+    portvalue = telnetstatus(username, telnetip, telnetports, int(loginport))
     dt["bandvalue"] = str(band)     # 拼接各个结果值
     dt["otherhost"] = str(hostip)
     dt["portconnect"] = str(portvalue)
